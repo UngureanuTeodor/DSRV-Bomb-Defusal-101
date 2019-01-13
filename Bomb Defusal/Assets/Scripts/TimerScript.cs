162374fba4252304e525b6564a2a3602aa425721
Dante <dante.ryuuzaki94@gmail.com>
2019-01-13 17:30:19 +0200

    added new bomb 3D model

---
+++ Bomb Defusal/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+    private float elapsedTime = 600;
+    public TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime -= Time.deltaTime;
+
+        int minutes = (int)elapsedTime / 60;
+        int seconds = (int)elapsedTime % 60;
+        string minutesText = (minutes < 10 ? "0" : "") + minutes.ToString();
+        string secondsText = (seconds < 10 ? "0" : "") + seconds.ToString();
+
+        textMesh.text = minutesText + ":" + secondsText;
+	}
+}
